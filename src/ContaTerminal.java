fd207f6b42dfdf33069fdd9e705a8f7c389391fa
Samuel <samuel.bozza0023@outlook.com>
2025-02-03 21:01:34 -0300

    Final Commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Digite uma mensagem:");
+        String lendo = ler.nextLine();
+
+        System.out.println(lendo);
     }
 }
